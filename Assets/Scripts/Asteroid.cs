644738355e2157d0eeffe08e2e21b6c9e6acc890
Nero Leung <nerolileung@gmail.com>
2020-03-11 11:50:27 +0000

    assignment assets
    literally just missing the starbox cubemap because it's 201mb

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Asteroid : MonoBehaviour
+{
+    public int scoreValue;
+    protected Duster player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreValue = 1;
+        player = FindObjectOfType<Player>().GetComponent<Duster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        player.ChangeScore(scoreValue);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Duster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Duster : MonoBehaviour
+{
+    private int score = 0;
+    private Text scoreText;
+    private GameObject modeIndicatorLeft;
+    private GameObject modeIndicatorRight;
+
+    public struct ColourMode
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GameObject.Find("Score Text").GetComponent<Text>();
+        modeIndicatorLeft = GameObject.Find("Left Mode");
+        modeIndicatorRight = GameObject.Find("Right Mode");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScore(int value)
+    {
+        score += value;
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
